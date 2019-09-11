a07e28e4f9c5feda757dbe9172a7cd1372f27cf0
xiaqian <xiaqian1999@gmail.com>
2019-09-11 11:24:15 -0400

    Initial Commit

---
+++ TIP_Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TIP_Calculator
+//
+//  Created by 潘玉霖 on 9/11/19.
+//  Copyright © 2019 xiaqian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
